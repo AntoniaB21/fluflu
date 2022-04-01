3066cfc91a37fcabe48d83db0936dc8c8dd64d28
= <antonia.balluais@mail-ecv.fr>
2022-04-01 13:12:00 +0200

    add profile screen

--- lib/screens/profile/index.dart
+++ lib/screens/profile/index.dart
@@ -5,42 +5,119 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int index = 0;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MyProfile'),
-        actions: [
-          GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, '/profile/update');
-            },
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Icon(Icons.edit),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(left: 30, top: 100, right: 30, bottom: 50),
+            height: 400,
+            width: 350,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30)
+              ),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 4,
+                  offset: Offset(
+                    0.0,
+                    4,
+                  ),
+                )
+              ],
             ),
+            child: Column(
+              children: <Widget>[
+                Stack(
+                  children: [
+                    Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30),
+                        ),
+                        image: DecorationImage(
+                          image: NetworkImage('https://images.unsplash.com/photo-1647436929276-43fa809c907c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: 90,
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Container(
+                              height: 100,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                                border: Border.all(
+                                    color: Colors.white, width: 3.0
+                                ),
+                                image: DecorationImage(
+                                  image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Column(
+                              children: [
+                                SizedBox(
+                                  height: 50,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 0),
+                                  child: Text('Antonia Balluais', style: TextStyle(fontWeight: FontWeight.bold)),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: 220,
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.school),
+                          contentPadding: EdgeInsets.symmetric(horizontal: 40),
+                          title: Text('ECV Digital', style: TextStyle(fontWeight: FontWeight.bold)),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.work),
+                          contentPadding: EdgeInsets.symmetric(horizontal: 40),
+                          title: Text('Développeur Back-End', style: TextStyle(fontWeight: FontWeight.bold)),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+
+            ),
+
+
           ),
+
         ],
       ),
-      body: Stack(
-      alignment: Alignment.center,
-       children: [
-         ProfileAvatarWidget(),
-         Positioned(
-           top: 00,
-           child: ProfilePicture()
-         )
-       ], 
-      )
     );
   }
 }
-Widget ProfileAvatarWidget() => Container(
-  color: Colors.black,
-  child :Image.network('https://images.unsplash.com/photo-1647436929276-43fa809c907c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80'),
-);
-
-Widget ProfilePicture() => CircleAvatar(
-  backgroundColor: Colors.green,
-  backgroundImage:NetworkImage(
-    'https://images.unsplash.com/photo-1579656592043-a20e25a4aa4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
-  ),
-);
