5fd3c15df9d11fd7cdb958e607a2e476bc9d6591
= <antonia.balluais@mail-ecv.fr>
2022-04-20 20:04:55 +0200

    update list show

---
+++ flutter_tp/lib/homepage.dart
@@ -0,0 +1,44 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+class HomePageShow extends StatelessWidget {
+  getShows() async {
+    var url;
+    url = await http.get(Uri.parse('https://api.tvmaze.com/shows'));
+
+    return convert.json.decode(url.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff191826),
+      appBar: AppBar(
+        backgroundColor: Color(0xff191826),
+        title:  Text(
+          'Shows',
+          style: TextStyle(fontSize: 30),
+        ),
+        elevation: 0.0,
+        centerTitle: false,
+      ),
+      body: FutureBuilder(
+        future: getShows(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error'),
+            );
+          }
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemBuilder: ();
+          }
+        },
+      )
+    );
+  }
+}
